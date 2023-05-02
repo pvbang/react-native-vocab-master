f4ea1b35940d5b5e687d2093dc1fd78f15aa94a1
pvbang <pvbang23092002@gmail.com>
2023-05-02 10:25:27 +0700

    js -> jsx

--- src/constants/string.js
+++ src/constants/string.js
@@ -1,7 +1,7 @@
 const strings = {
-    // GOOGLE_API_KEY: 'AIzaSyDLj0iPsxyG9EDNAMTZ-MX_CD5jkZc4gN0',
-    OPEN_AI_KEY: 'sk-AaxpvesrdNWAM9w50F'+'yZT3BlbkFJeBf5Ruc9mzNaU785sN9C',
-
+    // bỏ dấu cách đi   
+    GOOGLE_API_KEY: 'AIzaSyDLj0iPsxyG9 EDNAMTZ-MX_CD5jkZc4gN0',     
+    OPEN_AI_KEY: 'sk-7ljCsKvhfLQCx5tlm VKQT3BlbkFJFxZrmeYTBNa6IVVwxRol',
 };
 
 export default strings;
